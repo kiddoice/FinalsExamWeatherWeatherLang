a0c5daba7ac0f07abdcdf10eb7bfbc14212deb78
kiddoice <90586252+kiddoice@users.noreply.github.com>
2024-12-12 20:59:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,7 +105,7 @@ if input_data:
         st.write(f"Raw Prediction (Probability of Rain): {rain_probability:.2f}")
 
         # Adjust threshold if needed (e.g., using 0.5 for a balanced classification)
-        threshold = 0.7  # Adjusted to make the classification less strict
+        threshold = 0.5  # Adjusted to make the classification less strict
         if rain_probability > threshold:
             rain_prediction = 'Rain'
             identifier = f"Rain (Probability: {rain_probability:.2f})"
